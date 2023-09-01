451747e331f9493e40a5ca90c16d736706d2148e
Bryan Soto <bryan.soto@profesor.duoc.cl>
2023-09-01 00:16:05 +0000

    primer commmit, agrega archivo main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+  message = os.environ['MENSAJE']
+  return f'<h1>{message}</h1>'
+
+
+ip = os.environ.get('IP', '0.0.0.0')
+port = os.environ.get('PUERTO', '81')
+
+app.run(host=ip, port=port)
